87b914c80017e8d670fa36a5e849a4c773c5ee32
DaniloRadionica <cicvaricdanilo676@gmail.com>
2024-05-11 11:40:20 +0200

    Created task 2

--- Code9.API/Program.cs
+++ Code9.API/Program.cs
@@ -1,4 +1,6 @@
+using Code9.Domain.Handlers;
 using Code9.Domain.Interfaces;
+using Code9.Domain.Queries;
 using Code9.Infrastructure;
 using Code9.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,8 @@ builder.Services.AddDbContext<CinemaDbContext>(options =>
     options.UseInMemoryDatabase("Cinemas"));
 
 builder.Services.AddScoped<ICinemaRepository, CinemaRepository>();
+
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(GetAllCinemaQuery).Assembly));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
